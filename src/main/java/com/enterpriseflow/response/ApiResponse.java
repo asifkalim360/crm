d0436743c750b4e0f2c762edc569f53b65e31195
asifkalim360 <asifkalim360@gmail.com>
2026-02-08 15:38:01 +0530

    refactor: enhance ApiResponse with statusCode support

--- src/main/java/com/enterpriseflow/response/ApiResponse.java
+++ src/main/java/com/enterpriseflow/response/ApiResponse.java
@@ -9,13 +9,20 @@ import lombok.Data;
 @Builder
 public class ApiResponse<T> {
 
-    // T generic type hai, matlab data kisi bhi type ka ho sakta hai
+    // request success hua ya nahi.
     private boolean success;
 
+    // http status code store karenge.
+    private int statusCode;
+    
+    //user friendly message.
     private String message;
 
+    // T generic type hai, matlab data kisi bhi type ka ho sakta hai
+    //actual data
     private T data;
 
+    // response ka time
     private LocalDateTime timestamp;
 
 }
