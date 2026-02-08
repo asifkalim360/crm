c8687b73d50f7dcd668e93c9298d70dd0025dee5
asifkalim360 <asifkalim360@gmail.com>
2026-02-08 15:22:52 +0530

    add centralized constant classes for error and success messages

---
+++ src/main/java/com/enterpriseflow/constant/ErrorMessage.java
@@ -0,0 +1,14 @@
+package com.enterpriseflow.constant; 
+
+//yaha hum sab error messages centralize kar rahe hain
+//taaki project me kahin bhi hardcoded string na likhna pade
+
+public class ErrorMessage {
+	
+	public static final String USER_NOT_FOUND = "User not found"; 
+	public static final String USER_ALREADY_EXISTS = "User already exists";
+	public static final String INVALID_CREADENTIALS = "Invalid credentials"; 
+	public static final String ACCOUNT_NOT_ACTIVE = "Account is not active";
+	public static final String INTERNAL_SERVER_ERROR = "Something went wrong";
+	
+}
---
+++ src/main/java/com/enterpriseflow/constant/SuccessMessage.java
@@ -0,0 +1,10 @@
+package com.enterpriseflow.constant; 
+
+// Success Message bhi centralize kar rahe hain.
+public class SuccessMessage {
+	
+	public static final String USER_REGISTERED = "User registered succesfully"; 
+	public static final String LOGIN_SUCCESS = "Login successfully";
+	public static final String PASSWORD_CHANGED = "Password changed successfully";
+	
+}
